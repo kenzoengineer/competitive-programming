37995c52721b8e9c2161b80fb58c6301b9465e80
Ken Jiang <k47jiang@uwaterloo.ca>
2023-01-08 01:50:20 -0500

    Xiangbalao - Melo

--- 149 Max Points on a Line [H].py
+++ 149 Max Points on a Line [H].py
@@ -1,5 +1,9 @@
 # Math, Hashing, Geometry
 
+# TODO:
+# Handle duplicates
+# Fix double counting (set??)
+
 test = [[1,1],[[3,2],[5,3],[4,1],[2,3],[1,4]]]
 test = [[1,1],[1,1],[1,1],[1,2]]
 def maxPoints(points):
